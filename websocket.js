20c4e6acf6898cc319bb2f64ca75986f30c1a685
JSeoYoung <tsi04099@gmail.com>
2021-01-25 16:14:08 +0900

    add function check_status

--- websocket.js
+++ websocket.js
@@ -6,7 +6,7 @@ function init()
 {
   output = document.getElementById("output");
   setWebSocket();
-  setInterval(request_status,5000);
+  setInterval(request_status,10000);
 }
 
 function setWebSocket()
@@ -37,6 +37,7 @@ function onMessage(evt)
 
   if(message.sheet_type == 'sheet1'){
   	writeToScreen('<span style="color: green;">Response: ' + message.Date + '	' + message.Time + '	' + message.Temperature + '	' + message.HeartRate + '	' + message.Latitude + '	' + message.Longitude + '	' + message.Note +'</span>');
+  	check_status(message)
   }
   else if(message.sheet_type == 'sheet2'){
   	writeToScreen('<span style="color: green;">Response: ' + message.id + '	' + message.name + '	' + message.ip + '	' + message.port +'</span>');
@@ -83,6 +84,45 @@ function request_status()
   websocket.send(JSON.stringify(message));
 }
 
+function check_status(message)
+{
+  var temperature = Number(message.Temperature);
+  var heartrate = Number(message.HeartRate);
+  var temperature_check = "Green";
+  var heartrate_check = "Green";
+  var notify_msg;
+
+  //check temperature
+  if(temperature < 35 || temperature >38)
+	temperature_check = "Red";
+  else if(temperature >= 35 && temperature <=35.9)
+	temperature_check = "Yellow";
+  else if(temperature >= 37.6 && temperature <= 38)
+	temperature_check = "Yellow";
+
+  //check heartrate
+  if(heartrate < 65 || heartrate > 80)
+	heartrate_check = "Red";
+  else if(heartrate >=65 && heartrate <=70)
+	heartrate_check = "Yellow";
+  else if(heartrate >= 75 && heartrate <= 80)
+	heartrate_check = "Yellow";
+
+  //notification
+  if(temperature_check == "Red" && heartrate_check == "Red")
+	notify_msg = "Warning!!\n\nThere is a problem with body temperature and heart rate.";
+	//showNotification(notify_msg);
+  else if(temperature_check == "Red")
+	notify_msg = "Warning!!\n\nThere is a problem with body temperature.";
+	//showNotification(notify_msg);
+  else if(heartrate_check == "Red")
+	notify_msg = "Warning!!\n\nThere is a problem with heart rate.";
+	//showNotification(notify_msg);
+  writeToScreen(notify_msg);
+
+  writeToScreen('+++++++'+temperature_check+'  '+heartrate_check);
+}
+
 function writeToScreen(message)
 {
   var pre = document.createElement("p");
@@ -98,7 +138,7 @@ function showNotification(message)
           method:"createToast",
           parameters:{
               "sourceId" : "com.sample.websocket",
-              "message" : "sample test"
+              "message" : message
           },
           onSuccess: function (args) {
 
